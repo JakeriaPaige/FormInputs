c13299dffde13138102b157c0695f0eb5209cb54
JakeriaPaige <Jakeria.Paige@gmail.com>
2019-01-09 23:34:51 +0000

    Inkling or Octoling

--- flask_app.py
+++ flask_app.py
@@ -6,11 +6,21 @@ def main():
     # found in ../templates/
     return render_template("main_page.html")
 
+
+
+def Weapon(Weapon_Choice):
+    if Weapon_Choice == "Blaster" or Weapon_Choice == "Splattershot":
+        return "Inkling"
+    else:
+        return "Octoling"
+
 @app.route('/process_inputs', methods=['POST'])
 def process_inputs():
     name = request.form.get('input_name', '')
     dropdown = request.form.get('input_dropdown', '')
+
     select = request.form.get('input_select', '')
     freeform = request.form.get('input_freeform', '')
+    cephalpod = Weapon(dropdown)
     return render_template("main_page.html", input_data=dropdown,
-    output = "You're a Squid"
+            output = "You're an %s" % (cephalpod))
